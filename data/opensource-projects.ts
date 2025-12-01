cb8638eb37b7779bddb05bc0fdc643d31903d158
salmonco <xkqjsslsek80@naver.com>
2025-12-01 14:53:58 +0900

    feat: 데이터 추가

--- data/opensource-projects.ts
+++ data/opensource-projects.ts
@@ -16,4 +16,19 @@ export const OPENSOURCE_PROJECT: OpenSourceProject[] = [
       github: "https://github.com/salmonco/react-native-radar-chart",
     },
   },
+  {
+    id: 2,
+    title: "react-connect-line",
+    summary: "매칭 상태를 확인할 수 있는 React 요소 연결 라이브러리",
+    description:
+      "단순히 두 요소를 연결하는 패키지는 여럿 있었지만, 두 요소가 올바르게 매칭되었는지의 정보를 확인할 수 있는 패키지는 없었습니다. 이에 매칭 여부를 알려주는 State를 추가하도록 개선했습니다. 동시에 디자인이 용이하도록 커스텀 자유도를 높였습니다.",
+    achievements: ["월간 다운로드 수 60회"],
+    techStack: ["React", "TypeScript"],
+    links: {
+      web: "https://www.npmjs.com/package/react-connect-line",
+      ios: "",
+      android: "",
+      github: "https://github.com/salmonco/react-connect-line",
+    },
+  },
 ];
